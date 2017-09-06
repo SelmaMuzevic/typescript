d33ba898adcf963476fcd0cd716e869c604910a8
selma <selma@gmail.com>
2017-09-06 11:52:51 +0200

    rajout de todo router

--- main.ts
+++ main.ts
@@ -1,10 +1,9 @@
 
-
-import {Todo} from './todo';
+import {router} from './todo-router';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 
-const todo = new Todo();
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -12,30 +11,11 @@ app.use(bodyParser.urlencoded({
     extended:true
 }));
 
-const router = express.Router();
 
-// router permettant de recuperer les todos
-router.get('/', (req, res) => {
-    res.json(todo.lister())
-});
-
-// router permettant d'ajouter un todo
-    router.post('/', (req, res) => {
-        let nouveau = req.body.nouveau;
-        todo.ajouter(nouveau);
-        res.end('todo ajouté');
-});
-
-// router qui permets d'effacer un todo
-router.delete('/:suppr', (req, res) => {
-    let suppr = req.params.suppr;
-    todo.supprimer(suppr);
-    res.end('todo supprimé');
-});
-
-// on dit a l'application d'utiliser notre routeur sur 
-// le chemin /todo 
-// toutes les routes definies dedans devrons donc etre 
-// precedees de /todo
+//On dit à l'application d'utiliser notre routeur sur
+//le chemin /todo
+//Toutes les routes définies dedans devront donc être
+//précédées de /todo
 app.use('/todo', router);
+
 app.listen(3000);
